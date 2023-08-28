406b93fc32b8568e39dd057013f2a53d7098e13b
Allan-Binga <allanbinga73@gmail.com>
2023-08-28 18:23:13 -0400

    Initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Cards from './components/Cards';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Game of thrones</h1>
+      <Cards />
+    </div>
+  );
+}
+
+
+export default App;
